b9912f900941766597de72f6868b759ab966aab0
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-05-18 18:34:14 +0100

    updated the js file

---
+++ dec.js
@@ -0,0 +1,32 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  while (input > 0) {
+ binary = input % 2;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
