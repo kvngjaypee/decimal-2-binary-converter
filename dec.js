6146fa0d4fd2ea2da9380673e5a62ec390879eb7
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-05-19 14:36:28 +0100

    building in progress; created functions and an array

--- dec.js
+++ dec.js
@@ -1,3 +1,23 @@
+const callStack = [
+  'a(): returns "freeCodeCamp " + b()',
+  'b(): returns "is " + c()',
+  'c(): returns "awesome!"',
+];
+
+const a = () => {
+  return "freeCodeCamp " + b();
+};
+
+const b = () => {
+  return "is " + c();
+};
+
+const c = () => {
+  return "awesome!";
+};
+
+console.log(a());
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
@@ -5,8 +25,12 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
   let binary = "";
 
+  if (input === 0) {
+    binary = "0";
+  }
+
   while (input > 0) {
- binary = input % 2;
+    binary = (input % 2) + binary;
     input = Math.floor(input / 2);
   }
 
